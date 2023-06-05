d181b30fd1469701cc3a210cae09946c1e8d5e4c
Glenn Halls <gjhalls@gmail.com>
2023-06-05 16:58:49 +1000

    First screen

--- app/src/main/java/com/example/androidbasicswithcompose/MainActivity.kt
+++ app/src/main/java/com/example/androidbasicswithcompose/MainActivity.kt
@@ -3,13 +3,21 @@ package com.example.androidbasicswithcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.androidbasicswithcompose.ui.theme.AndroidBasicsWithComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +30,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    ComposeArticle()
                 }
             }
         }
@@ -30,17 +38,57 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
+fun ComposeArticle (modifier: Modifier = Modifier) {
+    Column(modifier = modifier.fillMaxSize()) {
+        HeadingBanner()
+        Heading()
+        Synopsis()
+        Content()
+    }
+}
+
+@Composable
+fun Heading(modifier: Modifier = Modifier) {
     Text(
-        text = "Hello $name!",
-        modifier = modifier
+        text = stringResource(R.string.heading),
+        fontSize = 24.sp,
+        modifier = modifier.padding(16.dp)
     )
 }
 
-@Preview(showBackground = true)
+@Composable
+fun Synopsis(modifier: Modifier = Modifier) {
+    Text(
+        text = stringResource(R.string.synopsis),
+        textAlign = TextAlign.Justify,
+        modifier = modifier.padding(start = 16.dp, end = 16.dp)
+    )
+}
+
+@Composable
+fun Content(modifier: Modifier = Modifier) {
+    Text(
+        text = stringResource(R.string.content),
+        textAlign = TextAlign.Justify,
+        modifier = modifier.padding(16.dp)
+    )
+}
+
+@Composable
+fun HeadingBanner(modifier: Modifier = Modifier) {
+    val image = painterResource(R.drawable.bg_compose_background)
+    Image(
+        painter = image,
+        contentDescription = null,
+    )
+}
+
+@Preview(
+    showBackground = true,
+    showSystemUi = true,)
 @Composable
 fun GreetingPreview() {
     AndroidBasicsWithComposeTheme {
-        Greeting("Android")
+        ComposeArticle()
     }
 }
