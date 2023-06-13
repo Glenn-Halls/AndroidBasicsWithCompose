6bb71d2834666825e965c4046477d2a46edf354b
Glenn Halls <gjhalls@gmail.com>
2023-06-13 13:11:08 +1000

    Lemonade App

--- app/src/main/java/com/example/androidbasicswithcompose/MainActivity.kt
+++ app/src/main/java/com/example/androidbasicswithcompose/MainActivity.kt
@@ -4,12 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -18,10 +20,12 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.androidbasicswithcompose.ui.theme.AndroidBasicsWithComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -29,50 +33,103 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             AndroidBasicsWithComposeTheme {
-                DiceRollerApp()
+                LemonadeStandApp()
             }
         }
     }
 }
 
 @Composable
-fun DiceWithButtonAndImage(modifier: Modifier = Modifier) {
-
-    var result by remember { mutableStateOf(1) }
-
-    val imageResource = when (result) {
-        1 -> R.drawable.dice_1
-        2 -> R.drawable.dice_2
-        3 -> R.drawable.dice_3
-        4 -> R.drawable.dice_4
-        5 -> R.drawable.dice_5
-        6 -> R.drawable.dice_6
-        else -> R.drawable.dice_6
-    }
+fun LemonadeStand(
+    imageId: Int,
+    descriptionId: Int,
+    stringId: Int,
+    clickAction: () -> Unit,
+    modifier: Modifier = Modifier) {
 
     Column(
-        modifier = modifier,
-        horizontalAlignment = Alignment.CenterHorizontally
+        modifier = modifier
+            .fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+
     ) {
-        Image(
-            painter = painterResource(imageResource),
-            contentDescription = result.toString()
+        Button(
+            modifier = modifier,
+            colors = ButtonDefaults.buttonColors(
+                containerColor = Color.LightGray
+            ),
+            shape = RoundedCornerShape(15),
+            onClick = clickAction
+        ) {
+            Image(
+                painter = painterResource(id = imageId),
+                contentDescription = stringResource(id = descriptionId)
+            )
+        }
+        Spacer(modifier = modifier.size(16.dp))
+        Text(
+            modifier = modifier,
+            text = stringResource(id = stringId),
+            fontSize = 18.sp
         )
-        Spacer(modifier = Modifier.height(16.dp))
-        Button(onClick = { result = (1..6).random() }) {
-            Text(stringResource(R.string.roll))
+    }
+}
+
+@Composable
+fun LemonLogic() {
+
+    var currentStage by remember { mutableStateOf(1) }
+    var numSqueezes by remember { mutableStateOf(0) }
+
+    when (currentStage) {
+        1 -> {
+            LemonadeStand(
+                imageId = R.drawable.lemon_tree,
+                descriptionId = R.string.lemon_tree,
+                stringId = R.string.tap_lemon,
+                { currentStage++ }
+            )
+        }
+        2 -> {
+            LemonadeStand(
+                imageId = R.drawable.lemon_squeeze,
+                descriptionId = R.string.lemon,
+                stringId = R.string.keep_tapping,
+                {
+                    numSqueezes++
+                    val squeezesNeeded = (2..4).random()
+                    if (numSqueezes >= squeezesNeeded) {
+                        numSqueezes = 0
+                        currentStage++
+                    }
+                }
+            )
+        }
+        3 -> {
+            LemonadeStand(
+                imageId = R.drawable.lemon_drink,
+                descriptionId = R.string.glass_lemonade,
+                stringId = R.string.tap_lemonade,
+                { currentStage++ }
+            )
+        }
+        4 -> {
+            LemonadeStand(
+                imageId = R.drawable.lemon_restart,
+                descriptionId = R.string.empty_glass,
+                stringId = R.string.tap_glass,
+                { currentStage = 1}
+            )
         }
     }
 }
 
 @Preview (
-    showSystemUi = true,
-    showBackground = true
+    showBackground = true,
+    showSystemUi = true
         )
 @Composable
-fun DiceRollerApp() {
-    DiceWithButtonAndImage(modifier = Modifier
-        .fillMaxSize()
-        .wrapContentSize(Alignment.Center)
-    )
+fun LemonadeStandApp() {
+    LemonLogic()
 }
